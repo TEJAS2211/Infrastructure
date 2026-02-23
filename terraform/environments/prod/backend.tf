349396d08ce1723df98f5d659be3397f34acf050
TEJAS2211 <131863535+TEJAS2211@users.noreply.github.com>
2026-02-23 15:40:44 +0530

    Update backend.tf

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "hospital-management-hackathon"
+    bucket  = "hospital-tf-state-839690183795"
     key     = "prod/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
